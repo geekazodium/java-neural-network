7505826c62fc46ab25d2fe56e685124546ab2779
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-05-21 15:04:31 +0800

    imported MNIST database
    I spelled it wrong in the previous commit

---
+++ src/main/java/com/geekazodium/handdrawndigitstuff/neuralnetwork/TrainingImage.java
@@ -0,0 +1,36 @@
+package com.geekazodium.handdrawndigitstuff.neuralnetwork;
+
+public class TrainingImage {
+    private final float[] data;
+    private final byte label;
+    public static final int height = 28;
+    public static final int width = 28;
+
+    public TrainingImage(byte[] data, byte label) {
+        this.data = new float[data.length];
+        for (int i = 0; i < data.length; i++) {
+            this.data[i] = normalizeByte(data[i])/256f;
+        }
+        this.label = label;
+    }
+    public void log(){
+        String asciiColor = " -=*%#";
+        float asciiColorLength = asciiColor.length();
+        for (int i = 0; i < width*height; i++) {
+            if(i%width==0) {
+                System.out.println();
+            }
+            System.out.print(" "+asciiColor.charAt((int)(this.data[i]*asciiColorLength)));
+        }
+        System.out.println();
+        System.out.println(label);
+    }
+
+    public static int normalizeByte(byte b){
+        return (b<0)?(int)b + 256:b;
+    }
+
+    public float[] getData() {
+        return this.data;
+    }
+}
