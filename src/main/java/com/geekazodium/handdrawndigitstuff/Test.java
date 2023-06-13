f8bb89f813a05cf7612dbf2bb6ebf8304d95ab69
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-06-13 15:12:51 +0800

    pretty print for gpu info

--- src/main/java/com/geekazodium/handdrawndigitstuff/Test.java
+++ src/main/java/com/geekazodium/handdrawndigitstuff/Test.java
@@ -1,22 +1,25 @@
 package com.geekazodium.handdrawndigitstuff;
 
+import com.geekazodium.handdrawndigitstuff.utils.ConsoleStylizer;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.opencl.*;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args){
-//        FloatBuffer a = BufferUtils.createFloatBuffer(10);
-//        FloatBuffer b = BufferUtils.createFloatBuffer(10);
-//        FloatBuffer out = BufferUtils.createFloatBuffer(a.capacity());
-//        a.put(new float[]{1f,2f,3f,4f,5f,6f,7f,8f,9f,10f});
-//        b.put(new float[]{1f,2f,3f,4f,5f,6f,7f,8f,9f,10f});
+        long[] computeDevices = getPlatformDevices(CL30.CL_DEVICE_TYPE_GPU);
+        System.out.println(Arrays.toString(computeDevices));
+        
+    }
 
-
+    private static long[] getPlatformDevices(int deviceType) {
         PointerBuffer platformsBuffer = BufferUtils.createPointerBuffer(64);
         IntBuffer platformCountBuffer = BufferUtils.createIntBuffer(1);
         int platformResult = CL30.clGetPlatformIDs(platformsBuffer,platformCountBuffer);
@@ -25,41 +28,79 @@ public class Test {
         int platformCount = platformCountBuffer.get();
         platformsBuffer.get(platforms);
 
+        List<Long> devicesList = new ArrayList<>();
+
         for(int i = 0; i<platformCount; i++){
-            getDevices(platforms[i]);
+            devicesList.addAll(getDevices(platforms[i],deviceType));
         }
+
+        long[] devices = new long[devicesList.size()];
+        for (int i = 0; i < devicesList.size(); i++) {
+            devices[i] = devicesList.get(i);
+        }
+        return devices;
     }
 
-    private static void getDevices(long platform) {
+    private static List<Long> getDevices(long platform,int deviceType) {
         PointerBuffer devicesBuffer = BufferUtils.createPointerBuffer(64);
         IntBuffer deviceCountBuffer = BufferUtils.createIntBuffer(1);
-        int deviceResult = CL30.clGetDeviceIDs(platform, CL30.CL_DEVICE_TYPE_ALL, devicesBuffer, deviceCountBuffer);
+        int deviceResult = CL30.clGetDeviceIDs(platform, deviceType, devicesBuffer, deviceCountBuffer);
         if(deviceResult != CL30.CL_SUCCESS){
             if(deviceResult == CL30.CL_INVALID_PLATFORM)throw new RuntimeException("Invalid platform");
             System.out.println("device incompatible #");
-            return;
+            return new ArrayList<>();
         }
         long[] devices = new long[64];
         int deviceCount = deviceCountBuffer.get();
         devicesBuffer.get(devices);
 
+        List<Long> devicesList = new ArrayList<>();
+
         for (int j = 0; j < deviceCount; j++) {
-            getDeviceInfo(devices[j],CL30.CL_DEVICE_NAME);
-            getDeviceInfo(devices[j],CL30.CL_DEVICE_VENDOR);
+            System.out.println(
+                    ConsoleStylizer.lineDivider(60, getDeviceInfoString(devices[j],CL30.CL_DEVICE_VENDOR)) +
+                    "\n"+
+                    getDeviceInfoString(devices[j],CL30.CL_DEVICE_NAME)+
+                    "\n"+
+                    ConsoleStylizer.formatByteSize(getDeviceInfoLong(devices[j], CL30.CL_DEVICE_GLOBAL_MEM_SIZE))+
+                    " global mem size\n"+
+                    ConsoleStylizer.formatByteSize(getDeviceInfoLong(devices[j], CL30.CL_DEVICE_GLOBAL_MEM_CACHE_SIZE))+
+                    " global mem cache size\n"+
+                    ConsoleStylizer.lineDivider(60, "platform:"+String.valueOf(platform))
+            );
+            devicesList.add(devices[j]);
         }
+        return devicesList;
     }
 
-    private static void getDeviceInfo(long deviceID,int DeviceInfo) {
-        ByteBuffer vendorNameBuffer = BufferUtils.createByteBuffer(256);
-        PointerBuffer nameLengthBuffer = BufferUtils.createPointerBuffer(1);
-        CL30.clGetDeviceInfo(deviceID,DeviceInfo,vendorNameBuffer,nameLengthBuffer);
-        int length = (int) nameLengthBuffer.get();
+    private static String getDeviceInfoString(long deviceID, int deviceInfo) {
+        ByteBuffer attribBuffer = BufferUtils.createByteBuffer(256);
+        PointerBuffer lengthBuffer = BufferUtils.createPointerBuffer(1);
+        CL30.clGetDeviceInfo(deviceID,deviceInfo,attribBuffer,lengthBuffer);
+        int length = (int) lengthBuffer.get();
 
         byte[] bytes = new byte[length-1];
-        vendorNameBuffer.get(bytes,0,length-1);
+        attribBuffer.get(bytes,0,length-1);
         String s = new String(bytes);
 
-        System.out.println(s);
+        return s;
+    }
+    private static byte[] getDeviceInfoBytes(long deviceID, int deviceInfo) {
+        ByteBuffer attribBuffer = BufferUtils.createByteBuffer(256);
+        PointerBuffer lengthBuffer = BufferUtils.createPointerBuffer(1);
+        CL30.clGetDeviceInfo(deviceID,deviceInfo,attribBuffer,lengthBuffer);
+        int length = (int) lengthBuffer.get();
+
+        byte[] bytes = new byte[length];
+        attribBuffer.get(bytes,0,length);
+
+        return bytes;
     }
 
+    private static long getDeviceInfoLong(long deviceID, int deviceInfo){
+        LongBuffer attribBuffer = BufferUtils.createLongBuffer(1);
+        PointerBuffer lengthBuffer = BufferUtils.createPointerBuffer(1);
+        CL30.clGetDeviceInfo(deviceID,deviceInfo,attribBuffer,lengthBuffer);
+        return attribBuffer.get();
+    }
 }
