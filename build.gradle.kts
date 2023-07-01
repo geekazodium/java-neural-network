5cf52d1ce42cb1b4d38a26d8c2936934cb626ac8
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-07-01 20:15:12 +0800

    Updated build script
    project now automatically selects the correct LWJGL-natives to use for device.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,26 @@ repositories {
 }
 
 val lwjglVersion = "3.3.2"
-val lwjglNatives = "natives-macos"
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } ->
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            else
+                "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }                ->
+            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+        arrayOf("Windows").any { name.startsWith(it) }                           ->
+            if (arch.contains("64"))
+                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+            else
+                "natives-windows-x86"
+        else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
